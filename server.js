5d52df7fa3d27a9b46647e517c0405a5299aa45e
imcrisam <imcrisam@gmail.com>
2020-12-12 13:54:59 -0500

    teste pass

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 debe importar como se observa en la siguiente linea, con el nombre del archivo js
 que contiene la logica */
 //const controller = require('./controller/nombredelcontrollador.js');
-const controller = require('./controller/controller.js');
+const controller = require('./controllers/MiController.js');
 const express = require('express');
 const db = require('./models');
 const app = express()
@@ -17,17 +17,17 @@ app.use(function (req, res, next) {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API ENDPOINTS
-/*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
-'/api/auth/signin'
-*/
+app.get('/', function (req, res) {
+    console.log("Estructura base del proyecto backend");
+    res.send("Estructura base del proyecto backend");
+});
 app.get('/api/users', (req, res) => {
     db.user.findAll().then(users => res.json(users))
 });
 app.post('/api/auth/signin', controller.signin);
-app.get('/', function (req, res) {
+/* app.get('/', function (req, res) {
     db.user.findAll().then(users => res.json(users))
-});
+}); */
 
 const port = 3000
 app.listen(port, () => {
