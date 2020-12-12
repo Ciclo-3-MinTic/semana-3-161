3374c3da92b24f5679dbadfa6ced1689a2b317a6
imcrisam <imcrisam@gmail.com>
2020-12-12 12:42:57 -0500

    init

--- server.js
+++ server.js
@@ -2,11 +2,12 @@
 debe importar como se observa en la siguiente linea, con el nombre del archivo js
 que contiene la logica */
 //const controller = require('./controller/nombredelcontrollador.js');
+const controller = require('./controller/controller.js');
 const express = require('express');
 const db = require('./models');
 const app = express()
 const bodyParser = require('body-parser');
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
@@ -20,10 +21,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /*se debe contar un una ruta por medio de método post para el inicio de sesión de la siguiente manera:
 '/api/auth/signin'
 */
-app.get('/', function(req, res) {
-    console.log("Estructura base del proyecto backend");
-    res.send("Estructura base del proyecto backend");
+app.get('/api/users', (req, res) => {
+    db.user.findAll().then(users => res.json(users))
 });
+app.post('/api/auth/signin', controller.signin);
+app.get('/', function (req, res) {
+    db.user.findAll().then(users => res.json(users))
+});
+
 const port = 3000
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`)
